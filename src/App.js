02c3e3e952a9c7a95ddd5689d3a91c2cad269405
Leonid <leonid.bgh@gmail.com>
2023-02-02 07:53:06 +0500

    Weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import './App.css'
 function App() {
+  const apiKey = 'f6296ceb552c03aa11d918d6369f030e'
+  const [weatherData, setWeatherData] = useState([{}])
+  const [city, setCity] = useState('')
+
+  const getWeather = (event) => {
+    if (event.key == 'Enter') {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data)
+          setCity('')
+        })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        type="text"
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+        onKeyDown={getWeather}
+        placeholder="Enter City..."
+        className="input"
+      />
+
+      {typeof weatherData.main === 'undefined' ? (
+        <div>
+          <p className='welcome'>Welcome Weather App! Enter a city to get weather of.</p>
+        </div>
+      ) : (
+        <div className="weather-data">
+          <p className="city">{weatherData.name}</p>
+          <p className="temp">{Math.round(weatherData.main.temp)}°C</p>
+          <p className="weather">{weatherData.weather[0].main}</p>
+        </div>
+      )}
+
+      {weatherData.cod === '404' ? <p>City not found</p> : <></>}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
